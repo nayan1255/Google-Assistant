19fd71d3d382f5af198534c4fa9647886bb5115e
Nayan Sunil Thorat <137385104+nayan1255@users.noreply.github.com>
2023-06-23 11:32:48 +0530

    Update service-installer.sh

--- scripts/service-installer.sh
+++ scripts/service-installer.sh
@@ -2,7 +2,7 @@
 #
 # Install systemd service files for running on startup.
 #
-# Copyright 2017 Google Inc.
+# Copyright 2017-2023 Google Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
